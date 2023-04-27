3bed887529c84bcdac93e0d755771344f6a7a1f0
goose <anrobbins18@gmail.com>
2023-04-27 10:39:53 -0400

    removed google maps API info

--- assets/js/google-maps.js
+++
@@ -1,9 +0,0 @@
-const URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyDShG28-MjGo4_Eh-Xcx_zXFwi3uD9LaNM&location=-33.8670522,151.1957362&radius=5000&type=supermarket";
-
-fetch(URL).then(data=> {
-  return data.json()
-}).then(jsonData => {
- console.log(jsonData.results)
-}).catch(error=> {
-  console.log(error);
-}) 
